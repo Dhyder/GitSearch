c7e41ac725f9a9e644c686bd4076e21f27e994c8
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-10-19 04:08:36 +0300

    generated pipe date update

--- src/app/date.pipe.ts
+++ src/app/date.pipe.ts
@@ -1,12 +1,29 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'date'
+  name: 'datePipe'
 })
 export class DatePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+
+  transform(value: string, type: string = ""): string {
+
+    let dd = value.substr(8, 2);
+    let MM = value.substr(5, 2);
+    let yyyy = value.substr(0, 4);
+    let date = `${dd}/${MM}/${yyyy}`;
+
+    // let time = '';
+
+    if (type != 'short') {
+        // let hh = value.substr(11, 2);
+        // let mm = value.substr(14, 2);
+        // let ss = value.substr(17, 2);
+        // time = `${hh}:${mm}:${ss}`;
+    }
+
+    return `${date}`;
+}
+    
   }
 
-}
