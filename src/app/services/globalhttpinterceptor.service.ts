a9a3263facde17f52cb20e6b3ff695974b534d47
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-10-19 04:04:02 +0300

    generated globalhttpinterceptor.service update

--- src/app/services/globalhttpinterceptor.service.ts
+++ src/app/services/globalhttpinterceptor.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class GlobalhttpinterceptorService {
-
-  constructor() { }
+import {Injectable} from "@angular/core";
+import {HttpEvent, HttpHandler, HttpInterceptor,HttpRequest,HttpResponse,HttpErrorResponse} from '@angular/common/http';
+import {Observable, of, throwError} from "rxjs";
+import {catchError, map} from 'rxjs/operators';
+import { Router } from "@angular/router";
+ 
+@Injectable()
+export class GlobalHttpInterceptorService implements HttpInterceptor {
+    
+  constructor(public router: Router) {
+  }
+ 
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+ 
+    return next.handle(req).pipe(
+      catchError((error) => {
+        console.log('error in intercept')
+        console.error(error);
+        return throwError(error.message);
+      })
+    )
+  }
 }
+ 
