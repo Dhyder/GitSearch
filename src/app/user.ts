49f65de6d376827bcd90465a47fb9f6303e1dd78
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-10-19 03:51:53 +0300

    generated class user update

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,11 @@
 export class User {
+    public login!: string;
+    public html_url!: string;
+    public avatar_url!: string;
+    public bio: any;
+    public name: any;
+    public location!: string;
+    followers: any;
+    following: any;
+    profile_created_on!: Date;
 }
