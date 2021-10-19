72d59767e470a9792f7c2e54f8ec0d33b80cf1c5
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-10-19 03:38:20 +0300

    Generated ExploreAppComponent and added Divs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExploreComponent } from './explore/explore.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExploreComponent
   ],
   imports: [
     BrowserModule,
